8650a4c4e71e86487d5916061e96d58e9fb62bfd
Tiffani Fox <teethefox@gmail.com>
2017-09-10 23:03:25 -0700

    Added form validation to Survey Assignment

--- survey.py
+++ survey.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 app = Flask(__name__)
+app.secret_key="Secrettttttt"
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/process", methods=['POST'])
 def process():
-    return render_template("process.html",name = request.form["name"], location = request.form["location"], language = request.form["language"], comment = request.form["comment"])
-    print name + location + language + comment
-    return redirect(url_for("/"))
+    if len(request.form['name']) < 1:
+        flash("Error : Name cannot be blank")
+        return redirect("/")
+    elif len(request.form['comment']) <1:
+        flash("Error: Comment cannot be blank")
+        return redirect("/")
+    elif len(request.form['comment']) > 120:
+        flash("Error: Comment cannot be longer than 120 characters.")
+        return redirect("/")
+    else:
+        return render_template("process.html",name = request.form["name"], location = request.form["location"], language = request.form["language"], comment = request.form["comment"])
+        print name + location + language + comment
+    return redirect("/")
 app.run(debug=True)
